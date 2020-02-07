79371c3cbff16188f1a0042403b6965df869ecc8
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-07 16:15:17 -0500

    more job card data

--- src/Components/Dashboard/JobCard.jsx
+++ src/Components/Dashboard/JobCard.jsx
@@ -5,8 +5,12 @@ function Job(props) {
 
     return(
         <div className="jobCard">
-          <h1>props.job.company</h1>
-          <h1>props.job.position</h1>
+          <h1>{props.job.company}</h1>
+          <h1>{props.job.position}</h1>
+          <h2>Applied on {props.job.appDate}</h2>
+          <h2>{props.job.method}</h2>
+          <h3>Notes: {props.job.notes}</h3>
+          <button>EDIT</button>
         </div>
         )
     }
