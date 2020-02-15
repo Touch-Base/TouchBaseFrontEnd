b07475b93411acf80a86df28e4f4723ec5bb3d8e
Anthony Vigliotta <anthonyvigz@gmail.com>
2020-02-15 11:18:54 -0600

    setting up profile formik

--- src/Components/Dashboard/Profile.jsx
+++ src/Components/Dashboard/Profile.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import '../../Styling/dashboard/profile.scss'
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import Error from '../../helpers/Error';
 
 function Profile(props) {
     
@@ -13,6 +16,31 @@ function Profile(props) {
         position: '',
         about: ''
     }
+
+//  This validation schema comes from the Yup library, it checks
+//  the Formik values to make sure everything entered suits the database
+//  and that the passwords match
+
+    const validationSchema = Yup.object().shape({
+        firstName: Yup.string()
+        .min(1, "Must have a character")
+        .max(20, "Must be shorter than 20")
+        .required("Must enter a first name"),
+        lastName: Yup.string()
+        .min(1, "Must have a character")
+        .max(20, "Must be shorter than 20")
+        .required("Must enter a last name"),
+        email: Yup.string()
+        .email("Must be a valid email address")
+        .required("Must enter an email"),
+        password: Yup.string()
+        .min(8, "Password must be 8 characters or longer")
+        .required("Password is required"),
+        confirmPassword: Yup.string().oneOf(
+        [Yup.ref('password'), null],
+        'Passwords must match'
+        )
+    })
         
     const [user, setUser] = useState(dummyUser);
     
@@ -22,8 +50,9 @@ function Profile(props) {
         setUser({ ...user, [event.target.name]: event.target.value });
     }
     
-    const updateProfile = () => {
+    const updateProfile = (event) => {
         event.preventDefault();
+    }
 
     return(
         <div className="profilePage">
@@ -36,15 +65,83 @@ function Profile(props) {
                 <h2>About: </h2>
             </div>
             
-            <form className="editForm">
-              <input 
+    {/* These initial values make up the values necessary to complete the form,
+      we update these values using Formik properties. */}
+
+      <Formik 
+        initialValues={{ 
+          firstName: '', 
+          lastName: '', 
+          email: '', 
+          password: '', 
+          confirmPassword: '' 
+        }} 
+        validationSchema={validationSchema}
+        onSubmit={(values, {setSubmitting, resetForm}) => {
+          setSubmitting(true);
+
+          const { firstName, lastName, email, password } = values;
+
+          this.props.registerUser({
+            "firstname": firstName,
+            "lastname": lastName,
+            "email": email,
+            "password": password
+        })
+        // successful register prompts to main page 
+
+        .then(() => {
+            navigate('/dashboard/');
+              
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+        }}
+      >
+        {({ 
+          values, 
+          errors, 
+          touched, 
+          handleChange, 
+          handleBlur, 
+          handleSubmit, 
+          isSubmitting 
+        }) => (
+          <form onSubmit={handleSubmit} className="registerForm">
+            <div className="firstLast">
+              <div className="oneInput">
+                <input 
                   type="text" 
-                  value={user.firstname} 
-                  name="firstname" 
-                  placeholder="firstname" 
-                  onChange={changeHandler}
-               />
-        </div>
+                  id="firstName" 
+                  placeholder="FIRST NAME *" 
+                  name="firstName"
+                  onChange={handleChange}
+                  value={values.firstName}
+                  onBlur={handleBlur} 
+                  className={(touched.firstName && errors.firstName) ? "hasError" : "validInput"}
+                />
+                <Error touched={touched.firstName} message={errors.firstName} />
+              </div>
+              <div className="oneInput">
+                <input 
+                  type="text" 
+                  id="lastName" 
+                  placeholder="LAST NAME *" 
+                  name="lastName"
+                  onChange={handleChange}
+                  value={values.lastName}
+                  onBlur={handleBlur} 
+                  className={touched.lastName && errors.lastName ? "hasError" : "validInput"}
+                />
+                <Error touched={touched.lastName} message={errors.lastName} />
+              </div>
+              <button type="submit" disabled={isSubmitting}>REGISTER</button>
+              </div>
+          </form>
+        )}
+      </Formik>
+    </div>
         )
     }
 
