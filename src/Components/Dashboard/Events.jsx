e21d0d3bd4fde3db2e1a842c01254223e8f977b7
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-21 11:36:57 -0500

    created events page component

---
+++ src/Components/Dashboard/Events.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import '../../Styling/dashboard/networking.scss'
+import EventForm from './EventForm';
+import EventCard from './EventCard';
+
+
+function Events(props) {
+
+    return(
+        <div className="events">
+            <EventForm />
+            {props.events.map( event => {
+                return <EventCard event={event} />
+            })}
+        </div>
+        )
+    }
+
+
+const mapStateToProps = (state) => {
+    return {
+        firstname: state.user.firstname,
+        lastname: state.user.lastname,
+        email: state.user.email,
+        events: state.user.events
+    }
+  }
+  
+  export default(
+    connect(
+        mapStateToProps,
+        null
+    )(Networking)
+  );
