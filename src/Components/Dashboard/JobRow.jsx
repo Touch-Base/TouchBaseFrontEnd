eeff7f9996234c13c2756a74f65a72ef3aee729d
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-27 12:45:22 -0500

    changed styling path

--- src/Components/Dashboard/JobRow.jsx
+++ src/Components/Dashboard/JobRow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../../Styling/dashboard/jobs.scss';
+import '../../Styling/dashboard/jobrow.scss';
 import JobForm from './JobForm';
 import { TwitterPicker } from 'react-color';
 import Modal from './Modal';
