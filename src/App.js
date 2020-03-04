e36c52607178555aec7afe43433c0886dbc5a6c1
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-03-04 10:36:25 -0500

    added routes for landing page pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import Home from './Components/LandingPage/Home';
 import Register from './Components/LandingPage/Register';
 import Login from './Components/LandingPage/Login';
+import Teams from './Components/LandingPage/Teams';
+import Learn from './Components/LandingPage/Learn';
+import Blog from './Components/LandingPage/Blog';
+import Support from './Components/LandingPage/Support';
+import Pricing from './Components/LandingPage/Pricing';
 import ProtectedRoutes from './Components/Auth/ProtectedRoutes';
 
 function App() {
@@ -15,6 +20,11 @@ function App() {
           <Home path="/" />
           <Register path="register" />
           <Login path="login" />
+          <Teams path="teams" />
+          <Learn path="learn" />
+          <Blog path="blog" />
+          <Support path="support" />
+          <Pricing path="pricing />
         </LandingPage>
         <ProtectedRoutes path="dashboard/*" />
       </Router>
