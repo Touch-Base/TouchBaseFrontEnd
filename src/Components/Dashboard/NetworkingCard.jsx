dbbf0d7ed5b3ac0a38d2afa82ac0412ccadc9b49
anthonyvigz <anthonyvigz@gmail.com>
2020-03-05 18:18:54 -0500

    setting up connectionc ards

--- src/Components/Dashboard/NetworkingCard.jsx
+++ src/Components/Dashboard/NetworkingCard.jsx
@@ -5,7 +5,20 @@ function Connection(props) {
 
     return(
         <div className="connectionCard">
-          <h1>{props.connection.firstname} {props.connection.lastname}</h1>
+          <h1>{props.connection.firstname.toUpperCase()} {props.connection.lastname.toUpperCase()}</h1>
+          <h5>{props.connection.company.toUpperCase()}</h5>
+          <h4>{props.connection.title}</h4>
+          <div className="cnxContact">
+            <div className="cnxPhone">
+              <i className="fas fa-phone"></i>
+              <h4>{props.connection.phone}</h4>
+            </div>
+            <div className="cnxEmail">
+              <i className="fas fa-at"></i>
+              <a href={props.connection.email}>{props.connection.email}</a>
+            </div>
+          </div>
+          <h4>{props.connection.notes}</h4>
         </div>
         )
     }
