6939dd54590dbb8399477ae7eb751889eafb084f
anthonyvigz <anthonyvigz@gmail.com>
2020-03-05 20:08:24 -0500

    finishing connection card styling

--- src/Components/Dashboard/NetworkingCard.jsx
+++ src/Components/Dashboard/NetworkingCard.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../Styling/dashboard/networking.scss'
 
 function Connection(props) {
+
+    const [ notes, setVisibility ] = useState(false);
+
+    const showNotes = event => {
+      event.preventDefault();
+
+      setVisibility(!notes);
+    }
 
     return(
         <div className="connectionCard">
           <h1>{props.connection.firstname.toUpperCase()} {props.connection.lastname.toUpperCase()}</h1>
           <h5>{props.connection.company.toUpperCase()}</h5>
-          <h4>{props.connection.title}</h4>
+          <h4>{props.connection.title.toUpperCase()}</h4>
           <div className="cnxContact">
-            <div className="cnxPhone">
+            <div className="cnxIcons">
               <i className="fas fa-phone"></i>
-              <h4>{props.connection.phone}</h4>
-            </div>
-            <div className="cnxEmail">
               <i className="fas fa-at"></i>
+            </div>
+            <div className="divider"></div>
+            <div className="cnxAddresses">
+              <h4>{props.connection.phone}</h4>
               <a href={props.connection.email}>{props.connection.email}</a>
             </div>
           </div>
-          <h4>{props.connection.notes}</h4>
+
+          {/* notes pop up */}
+          <button className="cnxNotesButton" onClick={showNotes}>
+            <i className="fas fa-quote-left">&nbsp;</i>    
+            <i className="fas fa-quote-right"></i>
+          </button>
+          {notes ? 
+            <p className="cnxNotes">{props.connection.notes}</p>
+          : null }
         </div>
         )
     }
