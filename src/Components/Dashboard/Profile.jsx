25fcfa3db2ebbac3cd2dcbe2c1b019d39224e8dc
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-03-11 13:16:37 -0400

    set up proper profile jsx with new classes

--- src/Components/Dashboard/Profile.jsx
+++ src/Components/Dashboard/Profile.jsx
@@ -29,36 +29,31 @@ function Profile(props) {
     return (
         <div className="profilePage">
             <div className="profileBlock">
-                <div className="imageAndName">
+                <div className="imageAndInfo">
                     <img src="https://image.shutterstock.com/image-photo/portrait-young-beautiful-cute-cheerful-260nw-666258808.jpg" width="200px" alt="portrait" />
-                    <div className="locationEmail">
-                        <div className="infoBox">
-                            <i className="fas fa-at"></i>
-                            <div className="infoText">
-                                <h5 className="typeTitle">EMAIL</h5>
-                                <h5 className="userEmail"> {props.email}</h5>
-                            </div>
-                        </div>
-                        <div className="infoBox">
-                            <i class="fas fa-map-marker-alt"></i>
-                            <div className="infoText loc">
-                                <h5 className="typeTitle">LOCATION</h5>
-                                <h5 className="userLocation"> {props.location || "Location"}</h5>
-                            </div>
-                        </div>
+                    <div className="mainSummary">
+                        <h2 className="positionTitle">{props.position || "Position"}</h2>
+                        <h2 className="fullName">{props.firstname} {props.lastname}</h2>
+                        <h2>Summary: {props.summary || "Summary"}</h2>
+                    </div>
+                </div>
+                <div className="profileContact">
+                    <div className="profileContactSec">
+                        <i className="fas fa-at"></i>
+                        <h5 className="userEmail"> {props.email}</h5>
+                    </div>
+                    <div className="profileContactSec">
+                        <i className="fas fa-map-marker-alt"></i>
+                        <h5 className="userLocation"> {props.location || "Location"}</h5>
+                    </div>
+                    <div className="profileContactSec">
+                        <i className="fab fa-pagelines"></i>
+                        <h5 className="age">{props.age || "Age"} Years Old</h2>
                     </div>
                 </div>
             </div>
-            <div className="infoSummary">
-                <div className="namePosition">
-                    <h2 className="fullName">{props.firstname} {props.lastname}</h2>
-                    <h2 className="positionTitle">{props.position || "Position"}</h2>
-                    <h2 className="age">{props.age || "Age"} Years Old</h2>
-                </div>
-                <div className="summary">
-                    <h2>Summary: {props.summary || "Summary"}</h2>
-                </div>
-            </div>
+
+        
             <button className={visibleProfile ? "exOut" : "updateProfile"} onClick={showProfileForm}>
                 <i className={visibleProfile ? "fas fa-times" : "fas fa-plus"}></i>
             </button>
