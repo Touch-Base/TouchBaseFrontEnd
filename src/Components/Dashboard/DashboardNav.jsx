4c9f171ee65ef6fae51aee90c1cec9ed839fa11f
anthonyvigz <anthonyvigz@gmail.com>
2020-03-21 14:41:38 -0400

    added on hover effect to job card

--- src/Components/Dashboard/DashboardNav.jsx
+++ src/Components/Dashboard/DashboardNav.jsx
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
-import '../../Styling/dashboard/dashboardnav.scss';
-import { navigate, redirectTo } from '@reach/router';
-import navLogo from '../../img/touchbaseblack.png';
+import React, { useState } from "react";
+import "../../Styling/dashboard/dashboardnav.scss";
+import { navigate, redirectTo, Link } from "@reach/router";
+import navLogo from "../../img/touchbasewhite.png";
 
 function DashboardNav() {
-    
-    // this changes the value for the sign out warning
-    const [ warning, setWarning ] = useState(false);
-    
-    // adds a 2 second delay to signing out
-    const signOut = () => {
-        window.localStorage.clear();
-        setTimeout(() => navigate('/'), 1000);
-        };
+  // this changes the value for the sign out warning
+  const [warning, setWarning] = useState(false);
 
-    // change warning value
-    const warningHandler = event => {
-        event.preventDefault();
+  // adds a 2 second delay to signing out
+  const signOut = () => {
+    window.localStorage.clear();
+    setTimeout(() => navigate("/"), 1000);
+  };
 
-        setWarning(!warning)
-    }
+  // checks for active link
+  const isActive = ({ isCurrent }) => {
+    return isCurrent ? { id: "activeLink" } : {};
+  };
 
-    return (
-        <nav className="dashboardNav">
-            <a href="#">
-              <img src={navLogo} alt="navLogo" />
-            </a>
-            <a href="profile">Profile</a>
-            <a href="jobs">Jobs</a>
-            <a href="networking">Networking</a>
-            <a href="events">Events</a>
-            <button id="signOutButton" onMouseEnter={warningHandler} onMouseLeave={warningHandler} onClick={signOut}>
-                <i className="fas fa-sign-out-alt"></i>
-            </button>
-            <h3 className="signOut" style={warning ? {opacity: 1} : {opacity: 0}}>Sign Out?</h3>
-        </nav>
-    )
+  // change warning value
+  const warningHandler = event => {
+    event.preventDefault();
+
+    setWarning(!warning);
+  };
+
+  return (
+    <nav className="dashboardNav">
+      <a href="#">
+        <img src={navLogo} alt="navLogo" />
+      </a>
+      <Link getProps={isActive} to="profile">
+        Profile
+      </Link>
+      <Link getProps={isActive} to="jobs">
+        Jobs
+      </Link>
+      <Link getProps={isActive} to="networking">
+        Networking
+      </Link>
+      <Link getProps={isActive} to="events">
+        Events
+      </Link>
+      <button
+        id="signOutButton"
+        onMouseEnter={warningHandler}
+        onMouseLeave={warningHandler}
+        onClick={signOut}
+      >
+        <i className="fas fa-sign-out-alt"></i>
+      </button>
+      <h3 className="signOut" style={warning ? { opacity: 1 } : { opacity: 0 }}>
+        sign out?
+      </h3>
+    </nav>
+  );
 }
 
 export default DashboardNav;
--- src/Components/Dashboard/JobCard.jsx
+++ src/Components/Dashboard/JobCard.jsx
@@ -1,151 +1,176 @@
-import React, { useState, useEffect } from 'react';
-import '../../Styling/dashboard/jobs.scss';
-import JobForm from './JobForm';
-import { TwitterPicker } from 'react-color';
-import Modal from './Modal';
-import { connect } from 'react-redux';
-import { editJob } from '../../actions/index';
-
+import React, { useState, useEffect } from "react";
+import "../../Styling/dashboard/jobs.scss";
+import JobForm from "./JobForm";
+import { TwitterPicker } from "react-color";
+import Modal from "./Modal";
+import { connect } from "react-redux";
+import { editJob } from "../../actions/index";
 
 function JobCard(props) {
+  const [visible, setVisibility] = useState(false);
+  const [bgcolor, setColor] = useState(props.job.color || "rgb(186, 43, 214)");
+  const [picker, setShowPicker] = useState(false);
+  const [notes, setShowNotes] = useState(false);
 
+  // this controls the visibility of the modal for the form
 
-    const [ visible, setVisibility ] = useState(false);
-    const [ bgcolor, setColor ] = useState (props.job.color || "rgb(186, 43, 214)");
-    const [ picker, setShowPicker ] = useState(false);
-    const [ notes, setShowNotes ] = useState(false);
+  const showForm = event => {
+    event.preventDefault();
 
-    
-    // this controls the visibility of the modal for the form
-    
-    const showForm = event => {
-      event.preventDefault();
+    setVisibility(!visible);
+  };
 
-      setVisibility(!visible)
-    }
+  const deleteButton = event => {
+    event.preventDefault();
 
-    const deleteButton = event => {
-      event.preventDefault();
+    const id = props.job.id;
 
-      const id = props.job.id
+    props.removeJob(id);
 
-      props.removeJob(id)
+    setVisibility(false);
+  };
 
-      setVisibility(false)
-    }
+  useEffect(() => {
+    // checks to see if the job was updated
+    // and closes the edit box
+    setVisibility(false);
+  }, [props.job]);
 
-    useEffect(() => {
+  /// this handles the color change in the color picker
+  /// it also sends the color to the job on the server
+  const handleChangeComplete = color => {
+    setColor(color.hex);
+    const newColor = color.hex;
+    const payload = { color: newColor, id: props.job.id };
+    props.editJob(payload);
+  };
 
-      // checks to see if the job was updated
-      // and closes the edit box
-      setVisibility(false)
+  // this is the handler to show the notes
+  const handlesNotes = event => {
+    event.preventDefault();
 
-    }, [props.job]);
+    setShowNotes(!notes);
+  };
 
-    /// this handles the color change in the color picker
-    /// it also sends the color to the job on the server
-    const handleChangeComplete = (color) => {
-      setColor(color.hex);
-      const newColor = color.hex
-      const payload = { color: newColor, id: props.job.id }
-      props.editJob(payload)
-    };
+  // this is the handler to show the color picker
+  const handleClick = () => {
+    setShowPicker(!picker);
+  };
 
-    // this is the handler to show the notes
-    const handlesNotes = (event) => {
-      event.preventDefault();
+  // this closes both the color picker and notes
+  const handleClose = () => {
+    setShowPicker(false);
+    setShowNotes(false);
+  };
 
-      setShowNotes(!notes);
-    };
+  // this is the positioning for the color picker
+  const popover = {
+    position: "absolute",
+    zIndex: "2",
+    top: "200px"
+  };
+  const cover = {
+    position: "fixed",
+    top: "0px",
+    right: "0px",
+    bottom: "0px",
+    left: "0px"
+  };
 
-    // this is the handler to show the color picker
-    const handleClick = () => {
-      setShowPicker(!picker)
-    };
-  
-    // this closes both the color picker and notes
-    const handleClose = () => {
-      setShowPicker(false)
-      setShowNotes(false)
-    };
+  return (
+    <div className="jobCard" style={{ background: bgcolor }}>
+      <h1 className="jobCompany">{props.job.company.toUpperCase()}</h1>
+      <h1 className="jobTitle">{props.job.position}</h1>
+      <h2 className="appliedText">
+        Applied on: <span>{props.job.appDate}</span> via{" "}
+        <span>{props.job.method}</span>
+      </h2>
+      <h2 className={props.job.interview ? "interview" : "nointerview"}>
+        {props.job.interview ? "INTERVIEW REQUESTED" : "NO INTERVIEW"}
+      </h2>
 
-    // this is the positioning for the color picker
-    const popover = {
-      position: 'absolute',
-      zIndex: '2',
-      top: '200px'
-    }
-    const cover = {
-      position: 'fixed',
-      top: '0px',
-      right: '0px',
-      bottom: '0px',
-      left: '0px',
-    }
+      {/* these are the card corners for hover effect  */}
+      <div className="cardCorners">
+        <i
+          id="topRight"
+          style={{ color: bgcolor }}
+          className="fas fa-chevron-up"
+        ></i>
+        <i
+          id="topLeft"
+          style={{ color: bgcolor }}
+          className="fas fa-chevron-up"
+        ></i>
+        <i
+          id="bottomRight"
+          style={{ color: bgcolor }}
+          className="fas fa-chevron-up"
+        ></i>
+        <i
+          id="bottomLeft"
+          style={{ color: bgcolor }}
+          className="fas fa-chevron-up"
+        ></i>
+      </div>
 
-
-    return(
-        <div className="jobCard" style={{background: bgcolor}}>
-          <h1 className="jobCompany">{props.job.company.toUpperCase()}</h1>
-          <h1 className="jobTitle">{props.job.position}</h1>
-          <h2 className="appliedText">Applied on: <span>{props.job.appDate}</span> via <span>{props.job.method}</span></h2>
-          <h2 className={props.job.interview ? "interview" : "nointerview"}>
-            {props.job.interview ? "INTERVIEW REQUESTED" : "NO INTERVIEW"}
-          </h2>
-
-          {/* this is the color picker and the button */}
-          <button className="colorButton" onClick={handleClick}>
-            <i className="fas fa-palette"></i>
-          </button>
-        {picker ? <div style={popover}>
-          <div style={cover} onClick={handleClose}/>
-            <TwitterPicker 
-              color={bgcolor}
-              onChangeComplete={handleChangeComplete}
-            />
-        </div> : null }
-
-          {/* this is the edit button */}
-          <a target="_blank" rel="noopener noreferrer" className="jobLink" href={props.job.link}>
-            <i className="fas fa-link"></i>
-          </a>
-          <button className="editLink" onClick={showForm}>
-            <i className="fas fa-pencil-alt"></i>
-          </button>
-          
-          {/* this button shows and hides the notes for each card */}
-          <button className="notes" onClick={handlesNotes}>
-            <i className="fas fa-quote-left">&nbsp;</i>    
-            <i className="fas fa-quote-right"></i>
-          </button>
-          {notes ? <div style={popover}>
-          <div style={cover} onClick={handleClose}/>
-          <p>{props.job.notes}</p>
-        </div> : null }
-
-          {/* this is the modal for the edit form */}
-          <Modal visible={visible}>
-            <div className="jobForm">
-              <h1 className="editJobTitle">EDIT JOB</h1>
-              <JobForm initialValues={props.job} editing={true} id={props.job.id} />
-              <button className="closeButton" onClick={showForm}>
-                <i className="fas fa-times"></i>
-              </button>
-              <button className="deleteButton" onClick={deleteButton}>DELETE JOB</button>
-            </div>
-          </Modal>
+      {/* this is the color picker and the button */}
+      <button className="colorButton" onClick={handleClick}>
+        <i className="fas fa-palette"></i>
+      </button>
+      {picker ? (
+        <div style={popover}>
+          <div style={cover} onClick={handleClose} />
+          <TwitterPicker
+            color={bgcolor}
+            onChangeComplete={handleChangeComplete}
+          />
         </div>
-        )
-    }
+      ) : null}
 
-    const mapDispatchToProps = {
-      editJob: editJob    
-    }
-    
-    export default(
-      connect(
-          null,
-          mapDispatchToProps
-      )(JobCard)
-    );
-  
+      {/* this is the edit button */}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className="jobLink"
+        href={props.job.link}
+      >
+        <i className="fas fa-link"></i>
+      </a>
+      <button className="editLink" onClick={showForm}>
+        <i className="fas fa-pencil-alt"></i>
+      </button>
+
+      {/* this button shows and hides the notes for each card */}
+      <button className="notes" onClick={handlesNotes}>
+        <i className="fas fa-quote-left">&nbsp;</i>
+        <i className="fas fa-quote-right"></i>
+      </button>
+      {notes ? (
+        <div style={popover}>
+          <div style={cover} onClick={handleClose} />
+          <p>{props.job.notes}</p>
+        </div>
+      ) : null}
+
+      {/* this is the modal for the edit form */}
+      <Modal visible={visible}>
+        <div className="jobForm">
+          <h1 className="editJobTitle">EDIT JOB</h1>
+          <JobForm initialValues={props.job} editing={true} id={props.job.id} />
+          <button className="closeButton" onClick={showForm}>
+            <i className="fas fa-times"></i>
+          </button>
+          <button className="deleteButton" onClick={deleteButton}>
+            DELETE JOB
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+const mapDispatchToProps = {
+  editJob: editJob
+};
+
+export default connect(null, mapDispatchToProps)(JobCard);
