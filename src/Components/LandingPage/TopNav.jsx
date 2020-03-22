008541a90680ec4ddaa21fd837cd28ff53c09418
anthonyvigz <anthonyvigz@gmail.com>
2020-03-22 16:55:27 -0400

    more styling

--- src/Components/LandingPage/TopNav.jsx
+++ src/Components/LandingPage/TopNav.jsx
@@ -1,36 +1,27 @@
-import React from 'react';
-import '../../Styling/topnav.scss'
-import navLogo from '../../img/touchbaseblack.png';
+import React from "react";
+import "../../Styling/topnav.scss";
+import navLogo from "../../img/touchbaseblack.png";
 
 function TopNav(props) {
+  console.log(props);
 
-    console.log(props);
-    
-    if(props.uri === "/dashboard") {
-
-      return (
-        <div>
-          WOAH WOAH OWAH
-        </div>
-      );
-    }
-
-    else {
-      return(
-        <div className="topBar">
-            <a href="/">
-              <img src={navLogo} alt="navLogo" />
-            </a>
-            <nav>
-              <a href="/">Learn</a>
-              <a href="/">Blog</a>
-              <a href="/">Support</a>
-              <a href="/">Pricing</a>
-            </nav>
-          </div>
-      )
-    }
+  if (props.uri === "/dashboard") {
+    return <div>WOAH WOAH OWAH</div>;
+  } else {
+    return (
+      <div className="topBar">
+        <a href="/">
+          <img src={navLogo} alt="navLogo" />
+        </a>
+        <nav>
+          <a href="/">learn</a>
+          <a href="/">blog</a>
+          <a href="/">support</a>
+          <a href="/">pricing</a>
+        </nav>
+      </div>
+    );
   }
-  
-  export default TopNav;
-  
+}
+
+export default TopNav;
