2d910c4e368adb9aa03318b9a856b30de577a9ca
anthonyvigz <anthonyvigz@gmail.com>
2020-03-23 14:30:16 -0400

    basic event card styling

--- src/Components/Auth/ProtectedRoutes.jsx
+++ src/Components/Auth/ProtectedRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Redirect } from "@reach/router";
+import { Router, Redirect, Link, Location } from "@reach/router";
 import Dashboard from "../Dashboard/Dashboard";
 import Overview from "../Dashboard/Overview";
 import Profile from "../Dashboard/Profile";
@@ -7,6 +7,7 @@ import Jobs from "../Dashboard/Jobs";
 import Networking from "../Dashboard/Networking";
 import Events from "../Dashboard/Events";
 import "../../Styling/home.scss";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function ProtectedRoutes() {
   // eslint-disable-next-line consistent-return
@@ -21,6 +22,25 @@ function ProtectedRoutes() {
     }
   }
 
+  /// setting up pose group for component transitions
+  const FadeTransitionRouter = props => (
+    <Location>
+      {({ location }) => (
+        <TransitionGroup className="transition-group">
+          <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            {/* the only difference between a router animation and
+                any other animation is that you have to pass the
+                location to the router so the old screen renders
+                the "old location" */}
+            <Router location={location} className="router">
+              {props.children}
+            </Router>
+          </CSSTransition>
+        </TransitionGroup>
+      )}
+    </Location>
+  );
+
   const token = getToken();
 
   if (!token) {
@@ -28,7 +48,7 @@ function ProtectedRoutes() {
     return <Redirect to="/" noThrow />;
   } else {
     return (
-      <Router>
+      <FadeTransitionRouter>
         <Dashboard path="/">
           <Overview path="/" />
           <Profile path="/profile" />
@@ -36,7 +56,7 @@ function ProtectedRoutes() {
           <Networking path="/networking" />
           <Events path="/events" />
         </Dashboard>
-      </Router>
+      </FadeTransitionRouter>
     );
   }
 }
--- src/Components/Dashboard/EventCard.jsx
+++ src/Components/Dashboard/EventCard.jsx
@@ -1,88 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import '../../Styling/dashboard/events.scss'
-import Modal from './Modal';
-import EventForm from './EventForm';
-
+import React, { useState, useEffect } from "react";
+import "../../Styling/dashboard/events.scss";
+import Modal from "./Modal";
+import EventForm from "./EventForm";
 
 function Event(props) {
-    
-    /// we have to use 'evt' for the props event variable because it will confuse with 
-    /// actual jquery events
+  /// we have to use 'evt' for the props event variable because it will confuse with
+  /// actual jquery events
 
-    // this is the visibility for the descritpion
-    const [ description, setVisibility ] = useState(false);
+  // this is the visibility for the descritpion
+  const [description, setVisibility] = useState(false);
 
-    // this is the visibility for the modal
-    const [ form, setVisible ] = useState(false);
+  // this is the visibility for the modal
+  const [form, setVisible] = useState(false);
 
+  // this is the delete button
+  const deleteButton = event => {
+    event.preventDefault();
 
-    // this sets the visibility for notes
-    const showDescription = event => {
-      event.preventDefault();
+    const id = props.evt.id;
 
-      setVisibility(!description);
-    }
+    props.removeEvent(id);
 
-    // this is the delete button 
-    const deleteButton = event => {
-      event.preventDefault();
+    setVisibility(false);
+  };
 
-      const id = props.evt.id
+  // this sets the visibility for the form modal
+  const showForm = event => {
+    event.preventDefault();
 
-      props.removeEvent(id)
+    setVisible(!form);
+  };
 
-      setVisibility(false)
-    }
+  useEffect(() => {
+    // checks to see if the connection was updated
+    // and closes the edit box
+    setVisible(false);
+    setVisibility(false);
+    console.log("here here");
+  }, [props]);
 
-    // this sets the visibility for the form modal
-    const showForm = event => {
-      event.preventDefault();
-      
-      setVisible(!form)
-      
-    }
-
-    useEffect(() => {
-
-      // checks to see if the connection was updated
-      // and closes the edit box
-      setVisible(false);
-      setVisibility(false);
-      console.log("here here")
-
-    }, [props]);
-
-    return(
-        <div className="eventCard">
-          <h1>{props.evt.name.toUpperCase()}</h1>
+  return (
+    <div className="eventCard">
+      <h1>{props.evt.name.toUpperCase()}</h1>
+      <div className="evtRows">
+        <div className="evtRow">
+          <i className="fas fa-map-marker-alt"></i>
           <h5>{props.evt.location.toUpperCase()}</h5>
-          <h4>{props.evt.date.toUpperCase()}</h4>
-
-          {/* notes pop up */}
-          <button className="evtDescButton" onClick={showDescription}>
-            <i className="fas fa-quote-left">&nbsp;</i>    
-            <i className="fas fa-quote-right"></i>
-          </button>
-          {description ? 
-            <p className="evtDescription">{props.evt.description}</p>
-          : null }
-        <button className="editEvt" onClick={showForm}>
-            <i className="fas fa-pencil-alt"></i>
-        </button>
-        {/* // edit connection form modal */}
-        <Modal visible={form}>
-          <div className="editEvtForm">
-            <h3 className="editEvtTitle">EDIT EVENT</h3>
-            <button className="closeButton" onClick={showForm}>
-              <i className="fas fa-times"></i>
-            </button>
-            <EventForm initialValues={props.evt} editing={true} id={props.evt.id}  />
-            <button className="deleteEvtButton" onClick={deleteButton}>DELETE EVENT</button>
-          </div>
-        </Modal>
         </div>
-        )
-    }
+        <div className="evtRow">
+          <i className="far fa-calendar-alt"></i>
+          <h4>{props.evt.date.toUpperCase()}</h4>
+        </div>
+        <div className="descEvtRow">
+          <button className="editEvt" onClick={showForm}>
+            <i className="fas fa-pencil-alt"></i>
+          </button>
+          {description ? <p>{props.evt.description}</p> : <p>"hello"</p>}
+        </div>
+      </div>
 
-  
-  export default Event;
+      {/* // edit connection form modal */}
+      <Modal visible={form}>
+        <div className="editEvtForm">
+          <h3 className="editEvtTitle">EDIT EVENT</h3>
+          <button className="closeButton" onClick={showForm}>
+            <i className="fas fa-times"></i>
+          </button>
+          <EventForm
+            initialValues={props.evt}
+            editing={true}
+            id={props.evt.id}
+          />
+          <button className="deleteEvtButton" onClick={deleteButton}>
+            DELETE EVENT
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default Event;
