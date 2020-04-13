d2f7de0e26f34a619c9c48f16ad6d03486883c3b
anthonyvigz <anthonyvigz@gmail.com>
2020-04-12 20:20:40 -0400

    added demo video to website

--- src/Components/LandingPage/Demo.jsx
+++ src/Components/LandingPage/Demo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
+import "../../Styling/demo.scss";
+
 function Demo() {
   return (
     <motion.div
@@ -8,7 +10,14 @@ function Demo() {
       initial={{ opacity: 0, y: "-50%" }}
       className="contactBlock"
     >
-      DEMO PLACEHOLDER
+      <iframe
+        src="https://player.vimeo.com/video/406958066"
+        width="640"
+        height="360"
+        frameborder="0"
+        allow="autoplay; fullscreen"
+        allowfullscreen
+      ></iframe>
     </motion.div>
   );
 }
